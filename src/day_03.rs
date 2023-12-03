6cdbe8030a869d06af5548dcb00ca85c63ae055f
Antoine Pultier <45740+fungiboletus@users.noreply.github.com>
2023-12-03 13:46:55 +0100

    feat: ☃️ Day 3 part 2

--- src/day_03.rs
+++ src/day_03.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use ndarray::Array2;
 
 fn parse_input_data(data: &str) -> Array2<char> {
@@ -40,6 +42,7 @@ fn compute_nb_of_adjacent_symbols(data: &Array2<char>) -> Array2<usize> {
     return result;
 }
 
+#[inline]
 fn compute_number_from_matrix(
     data: &Array2<char>,
     row: usize,
@@ -52,21 +55,35 @@ fn compute_number_from_matrix(
         .fold(0, |acc, x| acc * 10 + x)
 }
 
-pub fn day_3_part_1(data: &str) -> i64 {
-    let data = parse_input_data(data);
-    //println!("{:?}", data);
+#[inline]
+fn fill_matrix_with_number(
+    matrix: &mut Array2<i64>,
+    row: usize,
+    col_start: usize,
+    length: usize,
+    number: i64,
+) {
+    for i in col_start..col_start + length {
+        matrix[(row, i)] = number;
+    }
+}
+
+fn identify_part_numbers(data: &Array2<char>) -> (Vec<i64>, Array2<i64>) {
     let dim = data.dim();
     let (rows, cols) = dim;
 
     let adjacent_symbols_matrix = compute_nb_of_adjacent_symbols(&data);
     //println!("{:?}", adjacent_symbols_matrix);
 
-    let mut sum: i64 = 0;
+    let mut part_numbers = Vec::<i64>::new();
 
-    // for each line
+    let mut part_numbers_matrix = Array2::<i64>::zeros(dim);
+
+    // for each row
     for i in 0..rows {
         let mut start_text_j: isize = -1;
         let mut has_seen_symbol = false;
+        // for each column
         for j in 0..cols {
             let c = data[(i, j)];
             let is_digit = c.is_digit(10);
@@ -92,8 +109,15 @@ pub fn day_3_part_1(data: &str) -> i64 {
                         start_text_j as usize,
                         text_len as usize,
                     );
+                    fill_matrix_with_number(
+                        &mut part_numbers_matrix,
+                        i,
+                        start_text_j as usize,
+                        text_len as usize,
+                        number,
+                    );
                     //println!("Found text: {:?}", number);
-                    sum += number;
+                    part_numbers.push(number);
                 }
                 start_text_j = -1;
                 has_seen_symbol = false;
@@ -105,22 +129,58 @@ pub fn day_3_part_1(data: &str) -> i64 {
             let number =
                 compute_number_from_matrix(&data, i, start_text_j as usize, text_len as usize);
             //println!("Found text: {:?}", number);
-            sum += number;
+            fill_matrix_with_number(
+                &mut part_numbers_matrix,
+                i,
+                start_text_j as usize,
+                text_len as usize,
+                number,
+            );
+            part_numbers.push(number);
+        }
+    }
+
+    return (part_numbers, part_numbers_matrix);
+}
+
+pub fn day_3_part_1(data: &str) -> i64 {
+    let data = parse_input_data(data);
+    let (part_numbers, _) = identify_part_numbers(&data);
+    return part_numbers.iter().sum();
+}
+
+pub fn day_3_part_2(data: &str) -> i64 {
+    let data = parse_input_data(data);
+    let (_, part_numbers_matrix) = identify_part_numbers(&data);
+
+    let mut sum: i64 = 0;
+
+    for ((row, col), symbol) in data.indexed_iter() {
+        if *symbol == '*' {
+            let mut adjacent_part_numbers = HashSet::<i64>::new();
+            for di in 0.max(row as isize - 1) as usize..=(data.dim().0 - 1).min(row + 1) {
+                for dj in 0.max(col as isize - 1) as usize..=(data.dim().1 - 1).min(col + 1) {
+                    let adjacent_part_number = part_numbers_matrix[(di, dj)];
+                    if adjacent_part_number > 0 {
+                        adjacent_part_numbers.insert(adjacent_part_number);
+                    }
+                }
+            }
+
+            if adjacent_part_numbers.len() == 2 {
+                sum += adjacent_part_numbers.iter().product::<i64>();
+            }
         }
     }
 
     return sum;
 }
 
-pub fn day_3_part_2(data: &str) -> i64 {
-    42
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const EXAMPLE_PART_1: &str = "467..114..
+    const EXAMPLE: &str = "467..114..
 ...*......
 ..35..633.
 ......#...
@@ -131,21 +191,10 @@ mod tests {
 ...$.*....
 .664.598..";
 
-    const EXAMPLE_PART_1B: &str = "467..114..
+    const EXAMPLE_B: &str = "467..114..
 ...*......
 ..35..633.
 ......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..";
-
-    const EXAMPLE_PART_2: &str = "467..114..
-...*......
-..35...633
-.......#..
 617*......
 .....+.58.
 ..592.....
@@ -155,12 +204,13 @@ mod tests {
 
     #[test]
     fn test_day_3_part_1() {
-        assert_eq!(day_3_part_1(EXAMPLE_PART_1), 4361);
-        assert_eq!(day_3_part_1(EXAMPLE_PART_1B), 4361);
+        assert_eq!(day_3_part_1(EXAMPLE), 4361);
+        assert_eq!(day_3_part_1(EXAMPLE_B), 4361);
+        // 528799
     }
 
     #[test]
     fn test_day_3_part_2() {
-        assert_eq!(day_3_part_2(EXAMPLE_PART_1), 42);
+        assert_eq!(day_3_part_2(EXAMPLE), 467835);
     }
 }
